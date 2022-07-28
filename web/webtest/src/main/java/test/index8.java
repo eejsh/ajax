66719e52d4cb2c4a44baa5ec11b3dad17ba66c23
eejang <105977239+eejsh@users.noreply.github.com>
2022-07-28 09:39:55 +0900

    Update index8.java

--- web/webtest/src/main/java/test/index8.java
+++ web/webtest/src/main/java/test/index8.java
@@ -1,40 +1,45 @@
 package test;
 
 import java.io.IOException;
-import javax.servlet.ServletConfig;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class index8 extends HttpServlet {
-
 	private static final long serialVersionUID = 1L;
        
     public index8() {
-    	
-    
+        
     }
-    
-	public void init(ServletConfig config) throws ServletException {
-		
-	
-	}
-	
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+	}
 	
-	}
-
+	
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		/*숙제
-		 * index8.html 에서 mid , 즉 id값을 전송 받아 
-		 * 아이디 park 외에는 모두 "가입되지 않은 고객입니다" 라는 메세지 출력 및 전페이치로 이동을 합니다. * <script> history.go(-1)..
-		 * park으로 데이터가 전송 받을 경우 "로그인 되셨습니다. 라는 메세지와 함께 nate.com으로 이동합니다.
-		 */
+		/* 숙제
+		   index8.html에서 id값을 전송 받아서
+		   아이디 park 외에는 모두 "가입되지 않은 고객 입니다." 라는 메세지  출력 및 전 페이지로 이동을 합니다.
+		   park으로 데이터가 전송 받을 경우   "로그인 되셨습니다." 라는 메세지와 함께 nate.com으로 이동 합니다.
+		  */
 		
-		
-	}
+		PrintWriter pr =  response.getWriter();
 
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		
+	    String mid = request.getParameter("mid").intern();
+	    
+     if(mid=="park") {
+    	 pr.print("<script>alert('로그인 되었습니다.');   location.href = 'http://www.nate.com'; </script>");
+    	      }else {
+    	 pr.print("<script>alert('가입되지 않은 고객 입니다.'); history.go(-1)</script>");
+  	     }
+
+  }
 }
