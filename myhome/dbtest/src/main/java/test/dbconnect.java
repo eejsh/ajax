8516d9d51de70c2dd69d75c779dcb8ecc84162dd
eejang <105977239+eejsh@users.noreply.github.com>
2022-08-02 18:09:43 +0900

    Add files via upload

--- myhome/dbtest/src/main/java/test/dbconnect.java
+++ myhome/dbtest/src/main/java/test/dbconnect.java
@@ -4,9 +4,20 @@ import java.sql.*;
 
 public class dbconnect {
 	//웹에 안띄움. 실제 로드는 index2.java
+	
+	//mysql 커넥터 방법.
 	public static Connection ct() throws ClassNotFoundException, SQLException {
 	String dbdrive = "com.mysql.jdbc.Driver";
 	String dburl = "jdbc:mysql://localhost:3306/webmail";
+
+	
+	// mysql 부분에 mssql로 변경해서 사용가능..
+	//mssql 커넥터 방법
+//	String dbdrive = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+//	String dburl = "jdbc:sqlserber://localhost:1433/webmail";
+	
+	
+	
 	String dbuser = "hong";
 	String dbpw = "a1234";
 	
--- myhome/dbtest/src/main/java/test/index4.java
+++ myhome/dbtest/src/main/java/test/index4.java
@@ -1,6 +1,9 @@
 package test;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -20,6 +23,42 @@ public class index4 extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	 request.setCharacterEncoding("utf-8");
+	 String pidx = request.getParameter("jidx1");
+	 String point = request.getParameter("data1");
+	 
+		
+		try {
+			
+			dbconnect db = new dbconnect();
+			Connection ct = db.ct();
+			//updata 적용 시 select 후에 updata가 진행 되어야 합니다.
+			//?는 1부터 시작!
+			String sql = "update joinmember set jpoint=? where jidx=?";
+			PreparedStatement ps = ct.prepareStatement(sql);
+			ps.setString(1, point);
+			ps.setString(2, pidx);		
+			int n =0;
+			//execute : select에만 사용
+			n = ps.executeUpdate();
+			if(n>0) {
+				System.out.println("정상적으로 수정 완료 되었습니다.");
+			}else {
+				throw new Exception("error");  //에러뜨면 catch에있는 문구 뜸.
+				
+			}
+			
+		}catch(Exception e) {  //에러
+			e.getMessage();
+			System.out.println("데이터 컬럼값 오류!");
+		}finally { //에러났을때 어떻게 할것인지?
+			
+			
+		}
+
+		
+		
+	
 	}
 
 }
