b0745c8b08297a4073795a573848c5310e6222fa
eejang <105977239+eejsh@users.noreply.github.com>
2022-08-03 22:39:40 +0900

    Update dbconfig.java

--- myhome/review_nc/src/main/java/config/dbconfig.java
+++ myhome/review_nc/src/main/java/config/dbconfig.java
@@ -8,10 +8,10 @@ public class dbconfig {
 //SQL 환경설정, 모듈
 	protected Connection con() throws ClassNotFoundException, SQLException{
 		
-		String dburl ="jdbc:mysql://localhost:3306/eejsh08";
+		String dburl ="jdbc:mysql://localhost:3306/아이디";
 		String dbdrive = "com.mysql.jdbc.Driver";
-	    String dbuser = "eejsh08";
-	    String dbpw ="hoho12!@";
+	    String dbuser = "아이디";
+	    String dbpw ="비번";
 		
 		Class.forName(dbdrive);
 		Connection con = DriverManager.getConnection(dburl, dbuser, dbpw);
