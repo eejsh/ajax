3209d0d1ce16ced4af61e9e4f8beeaed7187ffbd
eejang <105977239+eejsh@users.noreply.github.com>
2022-08-07 19:09:02 +0900

    Update review_nc.java

--- myhome/review_nc/src/main/java/config/review_nc.java
+++ myhome/review_nc/src/main/java/config/review_nc.java
@@ -2,37 +2,26 @@ package config;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.WriteAbortedException;
-import java.io.Writer;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
-@MultipartConfig(
-		
-		fileSizeThreshold = 1024*1024*1,
-		maxFileSize = 1024*1024*2,
-		maxRequestSize = 1024*1024*2
-		
-		)
+@MultipartConfig(   //파일 업로드 기능 처리 시 사용되는 어노테이션 속성      
+		fileSizeThreshold = 1024*1024*1,     //1MB (파일 업로드 시 사용되는 메모리 임시크기 용량)
+ 	maxFileSize = 1024*1024*2 ,   //2MB	  (업로드 파일 최대 크기값)
+	maxRequestSize = 1024*1024*2  //2MB   (파일 전송에 따른 최대 크기값) 최대 2메가바이트까지 첨부가능 설정
+	
+		//multipartconfig :파일 첨부 용량 설정..1~2MB까지 가능..
+)
 public class review_nc extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     public review_nc() {
     }
-	public void init(ServletConfig config) throws ServletException {
-	}
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	}
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	    request.setCharacterEncoding("utf-8");	
 	 
@@ -49,41 +38,38 @@ public class review_nc extends HttpServlet {
 	    String usertext = request.getParameter("textbox"); 
 	    
 
-	    
 	    // 파일 첨부기능
 	    
 	    
 	    Part part1 = request.getPart("file"); 
 	    String filename1 = part1.getSubmittedFileName(); //이름 확인
-	    
-	    
 	    String file1 = request.getServletContext().getRealPath("");
+	    
 	    String file2 = file1 + "upload/";   // 실제 저장 공간
 
 	    System.out.println(file1);
 	    System.out.println(file2);
 	
-	    String file3= file2.replace("/eejsh08/tomcat/webapps", ".");    //DB 경로 
-        String file4 = file3 + filename1;
-        
 	    
-	    for(Part p : request.getParts()){  //parts: 여러파일..
+	    String file4 = null;
+	    
+	    if(filename1=="") {
+	    	file4 = null;
+	    }else {
 	    	
-    	p.write(file2 + filename1);	
-    	}
-	   
+	   //	String file3 = file2.replace("C:\\myhome\\metadata\\plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\", ".\review_nc");
 	    
+	   // 	String file3 = file2.replace("\\Users\\jsh\\Desktop\\home\\metadata\\plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\", ".\review");
+		    	
+	    	String file3= file2.replace("/eejsh08/tomcat/webapps", ".");    //DB 경로 
+	    	file4 = file3 + filename1;
+	    	
+	    	for(Part p : request.getParts()){  //parts: 여러파일..
+	    		p.write(file2 + filename1);	
+	    	}
+	  
+	    }
 	    
-	    
-//	    Part filepart = request.getPart("file"); //사용자 입력 
-//	    String filename = filepart.getSubmittedFileName(); //파일명 가져오기
-//	    String paths = request.getServletContext().getRealPath("");  //경로
-//	    String upfile = paths +"upload/";
-//	    String up1 = upfile + filename ;
-//	    String up2 = up1.replace("/eejsh08/tomcat/webapps", ".");
-//	    response.getWriter().print(upfile + filename +"<br>");
-
-	    	    
 	    
 	    reviewok_module rm = new reviewok_module();
 	    
@@ -96,32 +82,21 @@ public class review_nc extends HttpServlet {
 	    list.add(usertext);
 	    list.add(file4); //파일명
 	    
-	    
 	    rm.insert(list);  //reviewok_module로 보냄.
-	   
 	   
 	    
 	    //모듈에서 넘어온 값 처리부분
 	    
 	    PrintWriter pr = response.getWriter();
 	    String ookk = rm.reok().intern();
-	   
-	   // System.out.println(rm);
-	   // System.out.println(ookk);
-	    
+	
 	    if(ookk=="ok") {
-//	    	pr.print("<script>alert('내용이 저장되었슴...'); location.href='./review_write.html' </script>");
 	    	pr.print("<script>alert('내용이 저장되었슴...'); location.href='./index.do' </script>");
 	    	
 	    }else {
 	    	pr.print("<script>alert('데이터가 올바르지 않습니다..ㅜ'); location.href='./review_write.html' </script>");
 	    	
 	    }
-
-	    
-	    
-	    
-	    
 	    
 	}
 
