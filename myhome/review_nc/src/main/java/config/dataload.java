0d7322a0b9401deedeb7f8cf35cc44763dbd93db
eejang <105977239+eejsh@users.noreply.github.com>
2022-08-07 19:02:40 +0900

    Update dataload.java

--- myhome/review_nc/src/main/java/config/dataload.java
+++ myhome/review_nc/src/main/java/config/dataload.java
@@ -1,5 +1,6 @@
 package config;
 
+import java.awt.DefaultKeyboardFocusManager;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -7,89 +8,79 @@ import java.util.ArrayList;
 
 public class dataload {
 
+	public String getMidx() {
+		return midx;
+	}
+
+	public void setMidx(String midx) {
+		this.midx = midx;
+	}
+
+	public String getMid() {
+		return mid;
+	}
+
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+
+	public String getMgrade() {
+		return mgrade;
+	}
+
+	public void setMgrade(String mgrade) {
+		this.mgrade = mgrade;
+	}
+
+	public String getMproduct() {
+		return mproduct;
+	}
+
+	public void setMproduct(String mproduct) {
+		this.mproduct = mproduct;
+	}
+
 	private String midx;
 	private String mid;
 	private String mgrade;
-	private String mtext;
-	private String indate;
-	private String imgs;
-	
-	
-	
-	//servlet setter
-	public void setmidx(String midx) {
-		this.midx = midx;
-		
-	}
-	
-	public void setmid(String mid) {
-		this.mid = mid;
-		
-	}
-	
-	public void setmgrade(String mgrade) {
-		this.mgrade = mgrade;
-		
-		
-	}
-	
+	private String mproduct;
 
-	////getter, jsp에서 가져와서 출력
-	
-	
-	public String setmidx() {
-	return this.midx ;
-		
-	}
-	
-	public String setmid() {
-	return	this.mid ;
-		
-	}
-	
-	public String setmgrade() {
-	return this.mgrade;
-		
-		
-	}
-	
-	public ArrayList<dataload> all_list(){ //배열값선언..
-		
-		
-		
+	// servlet setter
+
+	//// getter, jsp에서 가져와서 출력
+
+	public ArrayList<dataload> all_list() { // 배열값선언..
+
 		ArrayList<dataload> list = new ArrayList<>();
-		
+
 		try {
-			
+
 			dbconfig db = new dbconfig();
 			Connection ct = db.con();
-		
-			String sql = "select * from pd_review";
-			PreparedStatement ps  = ct.prepareStatement(sql);
+
+			String sql = "select midx, mid, mproduct, mgrade from pd_review;";
+			PreparedStatement ps = ct.prepareStatement(sql);
 			ResultSet rs = ps.executeQuery();
-			while(rs.next()) {
+
+			while (rs.next()) {
 				dataload dl = new dataload();
-				dl.setmidx(rs.getString("midx"));    //setmidx =>setter
-				dl.setmid(rs.getNString("mid"));
-				dl.setmgrade(rs.getString("mgrade"));
+				dl.setMidx(rs.getString("midx")); // setmidx =>setter
+				dl.setMid(rs.getString("mid"));
+				dl.setMproduct(rs.getString("mproduct"));
+
+				dl.setMgrade(rs.getString("mgrade"));
 				list.add(dl);
-				
+
 			}
+
 			System.out.println(list.toString());
-			
-		}catch(Exception e) {
-			
+
+		} catch (Exception e) {
+
 		}
-		
+
 		return list;
-		
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
