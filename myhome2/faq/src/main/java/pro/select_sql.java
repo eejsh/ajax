2a8a1acb4ed2ea674f4c30c151c778917dfa8b1a
eejang <105977239+eejsh@users.noreply.github.com>
2022-08-07 19:05:25 +0900

    Update select_sql.java

--- myhome2/faq/src/main/java/pro/select_sql.java
+++ myhome2/faq/src/main/java/pro/select_sql.java
@@ -4,89 +4,84 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class select_sql {
-	private String idx = null;
-	
+	private String inx = null; // 고유값
+	private String fcate = null; // 카테고리
+	private String faq_qc = null; // 제목
+	private String fwrite = null; // 글쓴
+	private String indate = null;
 
-	private String fcate = null;
-	private String fwrite = null;
-	private String faq_qc = null; //질문내용
-	private String faq_view = null;
-	private String faq_indate = null;
-	
-	public String getIdx() {
-		return idx;
+	public void setinx(String inx) {
+		this.inx = inx;
 	}
-	
-	
-	public String getFcate() {
-		return fcate;
-	}
-	public String getFwrite() {
-		return fwrite;
-	}
-	public String getFaq_qc() {
-		return faq_qc;
-	}
-	public String getFaq_view() {
-		return faq_view;
-	}
-	public String getFaq_indate() {
-		return faq_indate;
-	}
-	
 
-	public void setIdx(String idx) {
-		this.idx = idx;
-	}
-	
 	public void setFcate(String fcate) {
 		this.fcate = fcate;
 	}
-	public void setFwrite(String fwrite) {
-		this.fwrite = fwrite;
-	}
+
 	public void setFaq_qc(String faq_qc) {
 		this.faq_qc = faq_qc;
 	}
-	public void setFaq_view(String faq_view) {
-		this.faq_view = faq_view;
-	}
-	public void setFaq_indate(String faq_indate) {
-		this.faq_indate = faq_indate;
+
+	public void setFwrite(String fwrite) {
+		this.fwrite = fwrite;
 	}
 
-	public ArrayList<select_sql> viewdata() throws ClassNotFoundException, SQLException{
-		
+	public void setIndate(String indate) {
+		this.indate = indate;
+	}
+
+	public String getinx() {
+		return inx;
+	}
+
+	public String getFcate() {
+		return fcate;
+	}
+
+	public String getFaq_qc() {
+		return faq_qc;
+	}
+
+	public String getFwrite() {
+		return fwrite;
+	}
+
+	public String getIndate() {
+		return indate;
+	}
+
+	public ArrayList<select_sql> viewdata() throws ClassNotFoundException, SQLException {
+
 		ArrayList<select_sql> list = new ArrayList<>();
-		select_sql ss = new select_sql();
 		
 		dbcon db = new dbcon();
 		db.cafe24();
 		
 		Connection con = db.cafe24();
-		String sql= "select * from faq_board order by faq_indate asc" ; 
+		
+		String sql= "select * from faq_board;" ; 
+//		String sql = "select inx, fcate, fwrite, faq_qc, indate from faq_board; ";
+		
+		
 		PreparedStatement ps = con.prepareStatement(sql);
-		
 		ResultSet rs = ps.executeQuery();
-		while(rs.next()) {
-			ss.setIdx(rs.getString("idx"));
+	
+		while (rs.next()) {
+			select_sql ss = new select_sql();
+			ss.setinx(rs.getString("inx"));
 			ss.setFcate(rs.getString("fcate"));
-			ss.setFwrite(rs.getString("fwirte"));
+			ss.setFwrite(rs.getString("fwrite"));
 			ss.setFaq_qc(rs.getString("faq_qc"));
-			ss.setFaq_view(rs.getString("faq_view"));
-			ss.setFaq_indate(rs.getString("faq_indate"));
-		list.add(ss);
-		
+			ss.setIndate(rs.getString("indate"));
+			list.add(ss);
+			
+			
+	//System.out.println(list.toString());
+			
 		}
-		
-		return list; 
-		
+
+		return list;
+
 	}
-	
-	
-	
-	
-	
-	
-	
+
 }
