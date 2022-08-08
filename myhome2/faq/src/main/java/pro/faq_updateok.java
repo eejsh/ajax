94692a87427bc7a5f8f4cbd39f019023b7f5f995
eejang <105977239+eejsh@users.noreply.github.com>
2022-08-08 23:56:42 +0900

    Update faq_updateok.java

--- myhome2/faq/src/main/java/pro/faq_updateok.java
+++ myhome2/faq/src/main/java/pro/faq_updateok.java
@@ -1,11 +1,11 @@
 package pro;
 
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -33,16 +33,28 @@ public class faq_updateok extends HttpServlet {
 			dbcon db = new dbcon();
 			Connection con = db.cafe24();
 			
+			String inx = request.getParameter("inx");
 			String fcate = request.getParameter("fcate");
 			String faq_qc = request.getParameter("faq_qc");
 			String faq_aw = request.getParameter("faq_aw");
-		//	String faq_view = (String)request.getParameter("faq_view").intern();
-			String inx = request.getParameter("inx");
+			String faq_view = (String)request.getParameter("faq_view").intern();
 			
-			System.out.println(inx);
+			String faq_view2 = "N"; 
 			
-			String sql = "update faq_borad set fcate='"+fcate+"' , faq_qc='"+faq_qc+"', faq_aw='"+faq_aw+"'  where inx='"+inx+"' ";
-			//String sql = "update faq_borad set fcate= ?, faq_qc= ?, faq_aw =?  where inx= ?...;
+			if(faq_view=="Y") {
+				faq_view2 = "Y";
+			}else {
+				faq_view2 ="N";
+			}
+			
+			
+			System.out.println(faq_view2 + "ㅇㅇ?");
+			
+			
+			
+			
+			String sql = "update faq_board set faq_view='"+faq_view2+"', fcate='"+fcate+"',faq_qc='"+faq_qc+"',faq_aw='"+faq_aw+"' where inx='"+inx+"'";
+			
 			
 			PreparedStatement ps = con.prepareStatement(sql);
 			
@@ -51,6 +63,7 @@ public class faq_updateok extends HttpServlet {
 			int n =0;
 			
 			n=ps.executeUpdate();
+			
 			if(n > 0) {
 				pw.print("<script>"
 						+ "alert('FAQ 수정이 완료 되었습니다.');"
@@ -65,7 +78,7 @@ public class faq_updateok extends HttpServlet {
 		}catch(Exception e) {
 			
 			pw.print("<script>"
-					+ "alert('올바른 접근이 아닙니다.');"
+					+ "alert('올바른 접근이 아닙니다. [update]');"
 					+ "history.go(-1);"
 					+ "</script>");
 			
@@ -75,3 +88,4 @@ public class faq_updateok extends HttpServlet {
 	}
 
 }
+
